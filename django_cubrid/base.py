3b11d6218b8f717df60c5a225ce96a7771644b85
Casian Andrei <casian@zco.ro>
2018-09-06 21:09:13 +0300

    Adapt bulk_insert_sql for Django >= 1.9

--- django_cubrid/base.py
+++ django_cubrid/base.py
@@ -318,9 +318,15 @@ class DatabaseOperations(BaseDatabaseOperations):
     def max_name_length(self):
         return 64
 
-    def bulk_insert_sql(self, fields, num_values):
-        items_sql = "(%s)" % ", ".join(["%s"] * len(fields))
-        return "VALUES " + ", ".join([items_sql] * num_values)
+    if django.VERSION < (1, 9):
+        def bulk_insert_sql(self, fields, num_values):
+            items_sql = "(%s)" % ", ".join(["%s"] * len(fields))
+            return "VALUES " + ", ".join([items_sql] * num_values)
+    else:
+        def bulk_insert_sql(self, fields, placeholder_rows):
+            placeholder_rows_sql = (", ".join(row) for row in placeholder_rows)
+            values_sql = ", ".join("({0})".format(sql) for sql in placeholder_rows_sql)
+            return "VALUES " + values_sql
 
 
 class DatabaseWrapper(BaseDatabaseWrapper):
