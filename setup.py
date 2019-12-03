944e73d1acfd7b64ef91a83e1b87e2a2dfb35d78
Kisoo Han <kisoo-han@cubrid.com>
2019-12-03 12:33:41 +0900

    [APIS-807] 10.2 Conformance

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import os
 import sys
 
-print ('sys version', sys.version);
 if sys.version > '3':
     setup_file = "setup_3.py"
 else:
     setup_file = "setup_2.py"
 
-version = "10.2.0.0001"
+version = "10.0.0.0001"
 
 #os.system(setup_file)
 setup_fh = open(setup_file)
--- setup_2.py
+++ setup_2.py
@@ -60,6 +60,7 @@ if os_type == 'Windows':
     inc_dir_base = os.path.join(script_dir, "cci-src\\src\\base")
     inc_dir_broker = os.path.join(script_dir, "cci-src\\src\\broker")
     inc_dir_cci = os.path.join(script_dir, "cci-src\\src\\cci")
+    inc_dir_compat = os.path.join(script_dir, "cci-src\\src\\compat")
 
     if arch_type == 'x86':
         lnk_dir = os.path.join(script_dir, "cci-src\\win\\cas_cci\\Win32\\Release")
@@ -75,6 +76,7 @@ else:
     inc_dir_base = os.path.join(script_dir, "cci-src/src/base")
     inc_dir_broker = os.path.join(script_dir, "cci-src/src/broker")
     inc_dir_cci = os.path.join(script_dir, "cci-src/src/cci")
+    inc_dir_compat = os.path.join(script_dir, "cci-src/src/cci")
 
     if arch_type == 'x86':
         lnk_dir = os.path.join(script_dir, "cci-src/cci/.libs")
@@ -93,7 +95,7 @@ if os_type == 'Windows':
                 library_dirs=[lnk_dir, lnk_dir_ex],
                 libraries=["cas_cci", "libregex38a",
                            "ws2_32", "oleaut32", "advapi32"],
-                include_dirs=[inc_dir_base, inc_dir_cci, inc_dir_broker],
+                include_dirs=[inc_dir_base, inc_dir_cci, inc_dir_broker, inc_dir_compat],
                 sources=['python_cubrid.c'],
             )
         ]
@@ -107,7 +109,7 @@ else:
         ext_modules = [
             Extension(
                 name="_cubrid",
-                include_dirs=[inc_dir_base, inc_dir_cci, inc_dir_broker],
+                include_dirs=[inc_dir_base, inc_dir_cci, inc_dir_broker, inc_dir_compat],
                 sources=['python_cubrid.c'],
                 libraries=["pthread", "stdc++"],
                 extra_objects=[cci_static_lib]
