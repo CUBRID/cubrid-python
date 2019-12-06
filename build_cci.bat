d3025ba77f1400dce647768798ba1bf6755798eb
Kisoo Han <kisoo-han@cubrid.com>
2019-12-06 19:51:12 +0900

    Update build_cci.bat

--- build_cci.bat
+++ build_cci.bat
@@ -1,7 +1,7 @@
 echo arg is :%1%
 cd cci-src\win\cas_cci
 
-call "%VS90COMNTOOLS%vsvars32.bat"
+call "%VS140COMNTOOLS%vsvars32.bat"
 if "%1%"=="x86" (
 devenv cas_cci_v140_lib.vcxproj /build "release|x86"
 ) else ( 
