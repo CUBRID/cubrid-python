39c8839bcb82216e8781ba0be53b6fb6e54d5b71
KangBu Jeong <54855268+Kang-dot@users.noreply.github.com>
2022-01-12 09:31:48 +0900

    [APIS-889] Enhancement creation.py
    http://jira.cubrid.org/browse/APIS-889

--- django_cubrid/creation.py
+++ django_cubrid/creation.py
@@ -4,57 +4,36 @@ import time
 import subprocess
 import django
 
-if django.VERSION >= (1, 8):
-    from django.db.backends.base.creation import BaseDatabaseCreation
-else:
-    from django.db.backends.creation import BaseDatabaseCreation
+from django.db.backends.base.creation import BaseDatabaseCreation
+
 # The prefix to put on the default database name when creating
 # the test database.
 TEST_DATABASE_PREFIX = 'test_'
 
 
 class DatabaseCreation(BaseDatabaseCreation):
-    if django.VERSION < (1, 6):
-        def sql_for_inline_foreign_key_references(self, field, known_models, style):
-            "Return the SQL snippet defining the foreign key reference for a field"
-            qn = self.connection.ops.quote_name
-            if field.rel.to in known_models:
-                output = [style.SQL_KEYWORD('FOREIGN KEY') + ' ' + \
-                    style.SQL_KEYWORD('REFERENCES') + ' ' + \
-                    style.SQL_TABLE(qn(field.rel.to._meta.db_table)) + ' (' + \
-                    style.SQL_FIELD(qn(field.rel.to._meta.get_field(field.rel.field_name).column)) + ')' +
-                    self.connection.ops.deferrable_sql()
-                ]
-                pending = False
-            else:
-                # We haven't yet created the table to which this field
-                # is related, so save it for later.
-                output = []
-                pending = True
+    def sql_for_inline_foreign_key_references(self, field, known_models, style):
+        "Return the SQL snippet defining the foreign key reference for a field"
+        qn = self.connection.ops.quote_name
+        rel_to = field.rel.to
+        if rel_to in known_models or rel_to == model:
+            output = [style.SQL_KEYWORD('FOREIGN KEY') + ' ' + \
+                style.SQL_KEYWORD('REFERENCES') + ' ' + \
+                style.SQL_TABLE(qn(field.rel.to._meta.db_table)) + ' (' + \
+                style.SQL_FIELD(qn(field.rel.to._meta.get_field(field.rel.field_name).column)) + ')' +
+                self.connection.ops.deferrable_sql()
+            ]
+            pending = False
+        else:
+            # We haven't yet created the table to which this field
+            # is related, so save it for later.
+            output = []
+            pending = True
 
-            return output, pending
-    else:
-        def sql_for_inline_foreign_key_references(self, model, field, known_models, style):
-            qn = self.connection.ops.quote_name
-            rel_to = field.rel.to
-            if rel_to in known_models or rel_to == model:
-                output = [style.SQL_KEYWORD('FOREIGN KEY') + ' ' + \
-                    style.SQL_KEYWORD('REFERENCES') + ' ' + \
-                    style.SQL_TABLE(qn(field.rel.to._meta.db_table)) + ' (' + \
-                    style.SQL_FIELD(qn(field.rel.to._meta.get_field(field.rel.field_name).column)) + ')' +
-                    self.connection.ops.deferrable_sql()
-                ]
-                pending = False
-            else:
-                # We haven't yet created the table to which this field
-                # is related, so save it for later.
-                output = []
-                pending = True
-
-            return output, pending
+        return output, pending
 
     def sql_indexes_for_model(self, model, style):
-        """ 
+        """
         Returns the CREATE INDEX SQL statements for a single model.
         The reference coloum can't be indexed in CUBRID.
         """
@@ -70,11 +49,11 @@ class DatabaseCreation(BaseDatabaseCreation):
             output.extend(self.sql_indexes_for_fields(model, fields, style))
         return output
 
-    def _create_test_db(self, verbosity, autoclobber):
+    def _create_test_db(self, verbosity, autoclobber, keepdb=False):
         "Internal implementation - creates the test db tables."
         suffix = self.sql_table_creation_suffix()
 
-        if self.connection.settings_dict['TEST_NAME']:
+        if 'TEST_NAME' in self.connection.settings_dict:
             test_database_name = self.connection.settings_dict['TEST_NAME']
         else:
             test_database_name = TEST_DATABASE_PREFIX + self.connection.settings_dict['NAME']
@@ -82,10 +61,21 @@ class DatabaseCreation(BaseDatabaseCreation):
         qn = self.connection.ops.quote_name
 
         # Create the test database and start the cubrid server.
-        create_command = ["cubrid", "createdb" , "--db-volume-size=20M", "--log-volume-size=20M", test_database_name, "en_US.utf8"]
+        check_command = ["cubrid", "checkdb", test_database_name]
+        create_command = ["cubrid", "createdb" , "--db-volume-size=20M", "--log-volume-size=20M",
+                          "--log-page-size=4K", "--db-page-size=4K",test_database_name, "en_US.utf8"]
         start_command = ["cubrid", "server", "start", test_database_name]
         stop_command = ["cubrid", "server", "stop", test_database_name]
         delete_command = ["cubrid", "deletedb", test_database_name]
+
+        if keepdb:
+            # Check if the test database already exists, in case keepdb is True
+            try:
+                subprocess.run(check_command, check = True)
+                print("Database already exists")
+                return # nothing to do if tit already exists
+            except subprocess.CalledProcessError:
+                pass # go ahead and create it
 
         try:
             server_version = self.connection.get_server_version().split(".")
@@ -97,25 +87,35 @@ class DatabaseCreation(BaseDatabaseCreation):
             pass
 
         try:
-            subprocess.call(create_command)
+            cp = subprocess.run(create_command, capture_output = True)
+            sys.stdout.write(cp.stdout.decode())
+            sys.stderr.write(cp.stderr.decode())
+            cp.check_returncode()
             print('Created')
-            subprocess.call(start_command)
+            subprocess.run(start_command, check = True)
             print('Started')
+            subprocess.run(check_command, check = True)
+            cursor = self.connection.cursor()
 
         except Exception as e:
             sys.stderr.write("Got an error creating the test database: %s\n" % e)
             if not autoclobber:
-                confirm = raw_input("Type 'yes' if you would like to try deleting the test database '%s', or 'no' to cancel: " % test_database_name)
+                confirm = input("Type 'yes' if you would like to try deleting the test database '%s', or 'no' to cancel: " % test_database_name)
             if autoclobber or confirm == 'yes':
                 try:
                     if verbosity >= 1:
                         print("Destroying old test database...")
-                        subprocess.call(stop_command)
-                        subprocess.call(delete_command)
+                        subprocess.run(stop_command)
+                        subprocess.run(delete_command)
 
                         print("Creating test database...")
-                        subprocess.call(create_command)
-                        subprocess.call(start_command)
+                        cp = subprocess.run(create_command, capture_output = True)
+                        sys.stdout.write(cp.stdout.decode())
+                        sys.stderr.write(cp.stderr.decode())
+                        cp.check_returncode()
+                        print('Created')
+
+                        subprocess.run(start_command)
                 except Exception as e:
                     sys.stderr.write("Got an error recreating the test database: %s\n" % e)
                     sys.exit(2)
@@ -137,19 +137,6 @@ class DatabaseCreation(BaseDatabaseCreation):
         self.connection._commit()
         return count == 0
 
-    def destroy_test_db(self, old_database_name, verbosity=1):
-        """
-        Destroy a test database, prompting the user for confirmation if the
-        database already exists. Returns the name of the test database created.
-        """
-        if verbosity >= 1:
-            print("Destroying test database '%s'..." % self.connection.alias)
-        self.connection.close()
-        test_database_name = self.connection.settings_dict['NAME']
-        self.connection.settings_dict['NAME'] = old_database_name
-
-        self._destroy_test_db(test_database_name, verbosity)
-
     def _destroy_test_db(self, test_database_name, verbosity):
         "Internal implementation - remove the test db tables."
         # Remove the test database to clean up after
@@ -157,11 +144,9 @@ class DatabaseCreation(BaseDatabaseCreation):
         # to do so, because it's not allowed to delete a database while being
         # connected to it.
         cursor = self.connection.cursor()
-        self.set_autocommit()
+
         time.sleep(1) # To avoid "database is being accessed by other users" errors.
-        p = subprocess.Popen(["cubrid", "server", "stop", test_database_name])
-        ret = os.waitpid(p.pid, 0)[1]
-        p = subprocess.Popen(["cubrid", "deletedb", test_database_name])
-        ret = os.waitpid(p.pid, 0)[1]
+        subprocess.run(["cubrid", "server", "stop", test_database_name])
+        subprocess.run(["cubrid", "deletedb", test_database_name])
 
         self.connection.close()
