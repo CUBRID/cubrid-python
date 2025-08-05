226ae289bc86821595f4434a67cdde73b4731014
hwanyseo <hwanyseo@cubrid.com>
2025-08-05 12:22:59 +0900

    [APIS-1062] #Format length and unittest function issue in Python 3.10. http://jira.cubrid.org/browse/APIS-1062
    - Added define PY_SSIZE_T_CLEAN and changed 'int' to 'Py_ssize_t' for '#'format
    - changed function (failUnless to assertTrue) for unitest

--- python_cubrid.c
+++ python_cubrid.c
@@ -324,7 +324,8 @@ _cubrid_escape_string (PyObject * self, PyObject * args, PyObject * kwargs)
   static char *kwList[] = { "escape_string", "no_backslash_escapes", NULL };
   int no_backslash_escapes = -1;
   char *unescape_string = NULL, *escape_string = NULL;
-  int len = -1, res;
+  Py_ssize_t len = -1;
+  int res;
   T_CCI_ERROR error;
 
   PyObject *op;
@@ -1381,7 +1382,8 @@ _cubrid_ConnectionObject_escape_string (_cubrid_ConnectionObject * self,
 					PyObject * args)
 {
   char *unescape_string = NULL, *escape_string = NULL;
-  int len = -1, res;
+  Py_ssize_t len = -1;
+  int res;
   PyObject *op;
   T_CCI_ERROR error;
 
@@ -3132,7 +3134,9 @@ static PyObject *
 _cubrid_LobObject_write (_cubrid_LobObject * self, PyObject * args)
 {
   char *buf = NULL, *type = NULL;
-  int len, res, type_len = 0;
+  Py_ssize_t len = -1;
+  Py_ssize_t type_len = 0;
+  int res = -1;
   T_CCI_ERROR error;
 
   if (!PyArg_ParseTuple (args, "s#|s", &buf, &len, &type))
